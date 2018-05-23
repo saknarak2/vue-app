222e069688fc0f27f55e89574ceb1ba3314aecb6
sak <saknarak@gmail.com>
2018-05-23 15:30:45 +0700

    router

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>Hello Vue</div>
+  <div>
+    <h1>Hello Vue</h1>
+    <div>
+      <nuxt-link to="/about">About</nuxt-link>
+    </div>
+    <div>
+      <button @click="gotoAbout">GO TO ABOUT</button>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      id: 1,
-    }
+  methods: {
+    gotoAbout() {
+      this.$router.push('/about')
+      // this.$router.push({ path: '/about' })
+      // this.$router.push('/about?id=1')
+      // this.$router.push({ path: '/about', query: {id: 1} })
+      // this.$router.replace('/about')
+      // this.$router.replace({ path: '/about' })
+      // this.$router.replace('/about?id=1')
+      // this.$router.replace({ path: '/about', query: {id: 1} })
+      // this.$router.go(-1)
+    },
   },
 }
 </script>
